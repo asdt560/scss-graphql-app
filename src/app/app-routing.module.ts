8539458119a6151dbed7dd26aa57a19c4e65d3e2
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-07-21 17:26:17 -0300

    Create main page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MainComponent } from './main/main.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    title: 'Main'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
