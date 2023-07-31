2a51cdee546ece4bb6fd85f8cb01d791a0df244d
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-07-31 17:19:09 -0300

    Create set page

--- src/app/set/set.component.ts
+++ src/app/set/set.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { inject } from '@angular/core';
 import { CardsService } from '../cards.service';
+import { Set } from '../interfaces';
 
 @Component({
   selector: 'app-set',
@@ -12,11 +13,11 @@ export class SetComponent {
   constructor(private cardsService : CardsService) {}
   route : ActivatedRoute = inject(ActivatedRoute);
   name : string = this.route.snapshot.paramMap.get('name') || "Base Set"
-  cards : any[] = [];
+  p: number = 1
+  set : Set | null = null;
   ngOnInit() {
     this.cardsService.getSet(this.name).then((data : any) => {
-      this.cards = data;
-      console.log(this.cards)
+      this.set = data;
     });
   }
 }
